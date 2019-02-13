d2f610de6da1de2a279859d2071c5b84480ad1c9
mklebe <matthias.klebe@gmail.com>
2019-02-13 21:33:10 +0100

    Rolls out data table and runsList

--- src/pages/runs.js
+++ src/pages/runs.js
@@ -1,55 +1,29 @@
 import React from 'react'
 
+import {TOURNAMENT, FinishedRun, FinishedRunList} from '../model/Run'
+
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import RunsList from '../components/runsList'
+import DataTable from '../components/dataTable'
+
+const runs = new FinishedRunList()
+
+runs.addRun(new FinishedRun('The Great 10K', 'October 8, 2017', '00:58:37', TOURNAMENT.Run10K))
+runs.addRun(new FinishedRun('Oderpokallauf', 'May 27, 2018', '00:57:43', TOURNAMENT.Run10K))
+runs.addRun(new FinishedRun('Potsdamer Schlösserlauf', 'June 3, 2018', '02:10:38', TOURNAMENT.RunHalf))
+runs.addRun(new FinishedRun('Schlaubetal Triathlon', 'July 8, 2018', '01:23:04', TOURNAMENT.TriathlonSprint))
+runs.addRun(new FinishedRun('The Great 10K', 'October 10, 2018', '00:53:58', TOURNAMENT.Run10K))
+runs.addRun(new FinishedRun('Müggelsee-Halbmarathon', 'October 21, 2018', '02:00:13', TOURNAMENT.RunHalf))
 
 const RunsPage = () => (
   <Layout>
     <SEO title="Portfolio" keywords={[`gatsby`, `application`, `react`]} />
-    <RunsList title="Finished Runs" list={runs} />
+    <DataTable 
+      title="Finished Runs"
+      tableBody={runs.getBodyFields()}
+      tableHead={runs.getHeadFields()} 
+    />
   </Layout>
 )
 
 export default RunsPage
-
-const Tournament = Object.freeze({
-  Run10K: 'Run 10K',
-  RunHalf: 'Halfmarathon',
-  TriathlonSprint: 'Triathlon sprint distance',
-})
-
-class Run {
-  /**
-   * @param {String} name
-   * @param {String} date
-   * @param {String} tournament
-   */
-  constructor( name, date, tournament ) {
-    this.name = name
-    this.date = date
-    this.tournament = tournament
-  }
-}
-
-class FinishedRun extends Run {
-  /**
-   * @param {String} name
-   * @param {String} date
-   * @param {String} time
-   * @param {String} tournament
-   */
-  constructor(name, date, time, tournament) {
-    super( name, date, tournament )
-    this.time = time
-  }
-}
-
-const runs = [
-  new FinishedRun('The Great 10K', 'October 8, 2017', '00:58:37', Tournament.Run10K),
-  new FinishedRun('Oderpokallauf', 'May 27, 2018', '00:57:43', Tournament.Run10K),
-  new FinishedRun('Potsdamer Schlösserlauf', 'June 3, 2018', '02:10:38', Tournament.RunHalf),
-  new FinishedRun('Schlaubetal Triathlon', 'July 8, 2018', '01:23:04', Tournament.TriathlonSprint),
-  new FinishedRun('The Great 10K', 'October 10, 2018', '00:53:58', Tournament.Run10K),
-  new FinishedRun('Müggelsee-Halbmarathon', 'October 21, 2018', '02:00:13', Tournament.RunHalf)
-]
